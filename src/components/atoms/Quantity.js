a19b5590df8a0d0d958dfe7fed5bf46cc83b5bcd
AS-DEVELOPER <67372463+AS-DEVELOPER7@users.noreply.github.com>
2026-03-25 15:38:20 +0300

    Delete src directory

--- src/components/atoms/Quantity.js
+++
@@ -1,5 +0,0 @@
-export default function Quantity({ value }) {
-  return (
-    <span className="inline-flex items-center justify-center w-8">{value}</span>
-  );
-}
--- src/utils/money.js
+++
@@ -1,5 +0,0 @@
-import { CURRENCY } from "src/constants";
-
-export function money(n) {
-  return `${Number(n).toFixed(2)} ${CURRENCY}`;
-}
